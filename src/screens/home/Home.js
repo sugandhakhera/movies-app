e17ec44ef29d4ff6a9b58ec737994d050ecf9bad
sugandha <sugs.sehgal2006@gmail.com>
2018-10-03 19:59:04 +0530

    Added Home Files

---
+++ src/screens/home/Home.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './Home.css';
+
+class Home extends Component {
+    render(){
+        return (
+            <div> hurray home </div> 
+        )
+    }
+}
+
+export default Home ;
