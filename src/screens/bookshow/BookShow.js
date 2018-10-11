f52c3fe513813d81e86d86eaf0953371d2248a72
sugandha <sugs.sehgal2006@gmail.com>
2018-10-11 20:45:07 +0530

    Added Location to show page

--- src/screens/bookshow/BookShow.js
+++ src/screens/bookshow/BookShow.js
@@ -5,14 +5,35 @@ import Header from '../../common/header/Header';
 import Home from '../../screens/home/Home';
 import Typography from '@material-ui/core/Typography';
 import './BookShow.css'; 
+import language from '../../common/language';
+import location from '../../common/location';
+import showDate from '../../common/showDate';
+import showTime from '../../common/showTime';
+
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+//import Input from '@material-ui/core/Input';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 
 class BookShow extends Component {
+    constructor() {
+        super();
+        this.state = {
+            location: ""
+        }
+    }
 
 
-backToMovieDetailsHandler = () => {
+    backToMovieDetailsHandler = () => {
     ReactDOM.render(<Home />, document.getElementById('root'));
-}
+    }
 
+    locationChangeHandler = event => {
+        this.setState({ location: event.target.value });
+    }
 
     render() {
         return (
@@ -22,12 +43,29 @@ backToMovieDetailsHandler = () => {
                   <div className="back">
                     <Typography onClick={this.backToMovieDetailsHandler}>
                         &#60; Back to Movie Details
-                        </Typography>
-                </div>
+                    </Typography>
+                    <Card className="cardStyle">
+                        <CardContent>
+                        <Typography variant="headline" component="h2">
+                                BOOK SHOW
+                        </Typography><br />
 
-                <div>
-                    BOOK SHOW 
-                </div>      
+                            <FormControl required className="formControl">
+                                <InputLabel htmlFor="location">Choose Location:</InputLabel>
+                                <Select
+                                    value={this.state.location}
+                                    onChange={this.locationChangeHandler}>
+                                    {location.map(loc => (
+                                        <MenuItem key={"loc" + loc.id} value={loc.location}>
+                                            {loc.location}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+
+                        </CardContent>
+                   </Card>
+                </div> 
             </div>
         );
 
